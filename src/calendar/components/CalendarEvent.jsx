d458c1f3978275c1cf91a87a5d8bd3c7f2ac1a76
samirjabib <samirdev9669@gmail.com>
2022-11-30 09:52:12 -0500

    add events calendar

---
+++ src/calendar/components/CalendarEvent.jsx
@@ -0,0 +1,12 @@
+export const CalendarEvent = ({event}) => {
+
+    const { title, user} = event;
+
+    return(
+        <>
+            <strong>{title}</strong>
+            <strong>{user.name}</strong>
+        </>
+    )
+    
+}
